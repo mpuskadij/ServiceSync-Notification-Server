e8ce967424403c817575faf656d20498727ad047
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-30 12:46:36 +0100

    FTR-88: -removed send because sendEach is used

--- src/main/kotlin/hr/foi/servicesync/data/FirebaseMessagingManager.kt
+++ src/main/kotlin/hr/foi/servicesync/data/FirebaseMessagingManager.kt
@@ -24,8 +24,6 @@ class FirebaseMessagingManager(private val firebaseMessaging: FirebaseMessaging)
                             .build()
 
                         messages.add(message)
-
-                        firebaseMessaging.send(message)
                 }
             }
         }
