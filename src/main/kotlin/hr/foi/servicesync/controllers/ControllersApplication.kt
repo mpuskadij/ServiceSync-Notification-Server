7b33c9e1fc6a7c6b88374c61b671320785385be4
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-30 09:33:26 +0100

    FTR-88: -updated .gitignore with Spring Boot

---
+++ src/main/kotlin/hr/foi/servicesync/controllers/ControllersApplication.kt
@@ -0,0 +1,11 @@
+package hr.foi.servicesync.controllers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControllersApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControllersApplication>(*args)
+}
---
+++ src/test/kotlin/hr/foi/servicesync/controllers/ControllersApplicationTests.kt
@@ -0,0 +1,13 @@
+package hr.foi.servicesync.controllers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControllersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
